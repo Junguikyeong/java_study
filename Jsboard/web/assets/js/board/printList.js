cb0b60065d274e2df3923bf87028131856d95781
Junguikyeong <gunto4@naver.com>
2023-02-23 21:33:19 +0900

    Jsboard

---
+++ Jsboard/web/assets/js/board/printList.js
@@ -0,0 +1,40 @@
+let initPage = () => {
+    let temp = new URLSearchParams(window.location.search).get("pageNo");
+    let data;
+    if (temp == null) {
+        data = {"pageNo": 1}
+    } else {
+        data = {"pageNo": temp}
+    }
+    $.ajax({
+        url: "/board/printList",
+        method: "GET",
+        data: data,
+        success: (message) => {
+            let result = JSON.parse(message);
+            let array = JSON.parse(result.data);
+            array.forEach((b) => {
+                insertRow(b);
+            })
+        }
+    });
+}
+
+function insertRow(board) {
+
+    let tbody = $('tbody');
+    let pagination = $(tbody).children().last();
+    $(tbody).remove(pagination);
+    let tr = document.createElement("tr");
+    $(tr).addClass("table-danger");
+    $(tr).click(() => {
+        location.href = "/board/printOne.jsp?id="+board.id;
+    });
+    $(tr).append($(document.createElement("td")).text(board.id));
+    $(tr).append($(document.createElement("td")).text(board.title));
+    $(tr).append($(document.createElement("td")).text(board.writerNickname));
+    $(tr).append($(document.createElement("td")).text(board.entryDate));
+    $(tr).append($(document.createElement("td")).text(board.modifyDate));
+    $(tbody).append(tr);
+    $(tbody).append(pagination);
+}
---
+++ Jsboard/web/board/update_logic.jsp
@@ -0,0 +1,54 @@
+<%@ page import="model.UserDTO" %>
+<%@ page import="connector.ConnectionMaker" %>
+<%@ page import="connector.MySqlConnectionMaker" %>
+<%@ page import="controller.BoardController" %>
+<%@ page import="model.BoardDTO" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<%
+    request.setCharacterEncoding("UTF-8");
+    UserDTO logIn = (UserDTO) session.getAttribute("logIn");
+    if (logIn == null) {
+        response.sendRedirect("/index.jsp");
+    }
+
+    int id = Integer.parseInt(request.getParameter("id"));
+
+    ConnectionMaker connectionMaker = new MySqlConnectionMaker();
+    BoardController boardController = new BoardController(connectionMaker);
+    BoardDTO b = boardController.selectOne(id);
+
+    if (b.getWriterId() != logIn.getId()) {
+        response.sendRedirect("/board/printOne.jsp?id=" + id);
+    }
+
+    b.setTitle(request.getParameter("title"));
+    b.setContent(request.getParameter("content"));
+
+    boardController.update(b);
+
+    response.sendRedirect("/board/printOne.jsp?id=" + id);
+%>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
